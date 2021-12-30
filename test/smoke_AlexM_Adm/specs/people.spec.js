0d122112ea9ce8fa8463f3d83f631eb14b198530
AlexMokeev <alexmokeevmd@gmail.com>
2021-12-29 19:10:55 -0500

    added smoke_AlexM folder

---
+++ test/smoke_AlexM_Adm/specs/people.spec.js
@@ -0,0 +1,8 @@
+BASE_URL = https://enduring.herokuapp.com
+BASE_USER_EMAIL=your@email
+BASE_USER_PASSWORD=passw
+USER_EMAIL=student7.jsgroup2@gmail.com
+USER_PASSWORD=Te$t$tudent1
+ADMIN_EMAIL=valeriy.kan@gmail.com
+ADMIN_PASSWORD=Te$t$tudent1
+API_URL=https://enduring-server.herokuapp.com/v3/graphql
---
+++ test/smoke_AlexM_Adm/specs/userLogin.spec.js
@@ -0,0 +1,11 @@
+const LoginPage = require('../pageobjects/Login.page');
+const PublicationsPage = require('../pageobjects/Publications.page');
+const Creds = require('../../../test_data/credentials')
+
+describe('User Login', () => {
+    it('Should login with valid credentials', async () => {
+        await LoginPage.login(Creds.vkcreds.email, Creds.vkcreds.password);
+        await expect(PublicationsPage.pageTitle).toBeExisting().true;
+        await expect(PublicationsPage.pageTitle).toHaveText('publications');
+    });
+});
